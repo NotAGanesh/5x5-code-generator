d74c6b89db2f2bad66d276e73aabef7ff20f840e
Ganesh Patil <105713980+NotAGanesh@users.noreply.github.com>
2024-10-13 12:12:32 +0530

    -- v1.1 update

--- main.php
+++ main.php
@@ -1,21 +1,53 @@
 <?php
+
 function generate_code() {
     $allowed_chars = "2346789BCDFGHJKMNPQRTVWXYZ";
-    $codes = [];
+    $codes = array();
 
-    for ($i = 0; $i < 25; $i++) {  // Generate 25 codes
-        $code = implode("-", array_map(function() use ($allowed_chars) {
-            return substr(str_shuffle(str_repeat($allowed_chars, 5)), 0, 5);
-        }, range(1, 4))) . "-" . substr(str_shuffle(str_repeat($allowed_chars, 4)), 0, 4) . "Z";
-        $codes[] = $code;
+    while (count($codes) < 25) {  // Generate 25 codes
+        $code_parts = array();
+
+        // Generate the first four parts with 5 characters each
+        for ($i = 0; $i < 4; $i++) {
+            $part = '';
+            for ($j = 0; $j < 5; $j++) {
+                $part .= $allowed_chars[random_int(0, strlen($allowed_chars) - 1)];
+            }
+            $code_parts[] = $part;
+        }
+
+        // Generate the last part with 4 characters and append 'Z'
+        $last_part = '';
+        for ($j = 0; $j < 4; $j++) {
+            $last_part .= $allowed_chars[random_int(0, strlen($allowed_chars) - 1)];
+        }
+        $last_part .= 'Z';
+
+        // Join all parts together
+        $code = implode("-", $code_parts) . "-" . $last_part;
+
+        // Check for three consecutive repeated characters
+        $valid = true;
+        for ($i = 0; $i < strlen($code) - 6; $i++) {
+            if (substr($code, $i, 3) == substr($code, $i + 3, 3)) {
+                $valid = false;
+                break;
+            }
+        }
+
+        // If valid, add to the list of codes
+        if ($valid) {
+            $codes[] = $code;
+        }
     }
 
     return $codes;
 }
 
 // Generate and print 25 codes
-foreach (generate_code() as $code) {
+$codes = generate_code();
+foreach ($codes as $code) {
     echo $code . "\n";
 }
-?>
 
+?>
