a2c78765b07654dd4c8a7ba254682dad8ff58321
Ganesh Patil <105713980+NotAGanesh@users.noreply.github.com>
2024-10-13 12:13:55 +0530

    -- v1.1 code base change -- Three letters won't repeat

--- main.rb
+++ main.rb
@@ -1,18 +1,46 @@
-r
-require 'securerandom'
-
 def generate_code
   allowed_chars = "2346789BCDFGHJKMNPQRTVWXYZ"
   codes = []
 
-  25.times do  # Generate 25 codes
-    code = (1..4).map { SecureRandom.random_bytes(5).tr("\x00-\x7F", allowed_chars).chars.sample(5).join }.join("-") + "-" + SecureRandom.random_bytes(4).tr("\x00-\x7F", allowed_chars).chars.sample(4).join + "Z"
-    codes << code
+  while codes.length < 25  # Generate 25 codes
+    code_parts = []
+
+    # Generate the first four parts with 5 characters each
+    4.times do
+      part = ""
+      5.times do
+        part += allowed_chars[rand(allowed_chars.length)]
+      end
+      code_parts << part
+    end
+
+    # Generate the last part with 4 characters and append 'Z'
+    last_part = ""
+    4.times do
+      last_part += allowed_chars[rand(allowed_chars.length)]
+    end
+    last_part += "Z"
+
+    # Join all parts together
+    code = code_parts.join("-") + "-" + last_part
+
+    # Check for three consecutive repeated characters
+    valid = true
+    (0..(code.length - 7)).each do |i|
+      if code[i, 3] == code[i + 3, 3]
+        valid = false
+        break
+      end
+    end
+
+    # If valid, add to the list of codes
+    codes << code if valid
   end
 
   codes
 end
 
 # Generate and print 25 codes
-generate_code.each { |code| puts code }
-
+generate_code.each do |code|
+  puts code
+end
