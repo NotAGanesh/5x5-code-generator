f34b4d032aa88030a60e79bacdf52c580396230d
Ganesh Patil <105713980+NotAGanesh@users.noreply.github.com>
2024-10-13 12:10:30 +0530

    -- v1.1 code base change -- Three letters won't repeat

--- main.java
+++ main.java
@@ -1,49 +1,48 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CodeGenerator {
-    private static final String ALLOWED_CHARS = "2346789BCDFGHJKMNPQRTVWXYZ";
-    private static final int NUMBER_OF_CODES = 25;
-    private static final int SEGMENT_LENGTH = 5;
-    private static final int FINAL_SEGMENT_LENGTH = 4;
-    private static final char END_CHAR = 'Z';
+    public static List<String> generateCode() {
+        String allowedChars = "2346789BCDFGHJKMNPQRTVWXYZ";
+        List<String> codes = new ArrayList<>();
+        Random random = new Random();
 
-    // Singleton Random instance
-    private static final Random RANDOM = new Random();
-
-    public static ArrayList<String> generateCode() {
-        ArrayList<String> codes = new ArrayList<>();
-
-        for (int i = 0; i < NUMBER_OF_CODES; i++) {
+        while (codes.size() < 25) {  // Generate 25 codes
             StringBuilder code = new StringBuilder();
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 5; j++) {
+                    code.append(allowedChars.charAt(random.nextInt(allowedChars.length())));
+                }
+                code.append("-");
+            }
             for (int j = 0; j < 4; j++) {
-                code.append(generateRandomString(ALLOWED_CHARS, SEGMENT_LENGTH));
-                if (j < 3) {
-                    code.append("-");
+                code.append(allowedChars.charAt(random.nextInt(allowedChars.length())));
+            }
+            code.append("Z");
+
+            // Check for three consecutive repeated characters
+            boolean hasConsecutive = false;
+            for (int i = 0; i < code.length() - 6; i++) {
+                if (code.substring(i, i + 3).equals(code.substring(i + 3, i + 6))) {
+                    hasConsecutive = true;
+                    break;
                 }
             }
-            // Append final segment and end character
-            code.append("-");
-            code.append(generateRandomString(ALLOWED_CHARS, FINAL_SEGMENT_LENGTH));
-            code.append(END_CHAR);
-            codes.add(code.toString());
+
+            if (!hasConsecutive) {
+                codes.add(code.toString());
+            }
         }
 
         return codes;
     }
 
-    private static String generateRandomString(String allowedChars, int length) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            sb.append(allowedChars.charAt(RANDOM.nextInt(allowedChars.length())));
-        }
-        return sb.toString();
-    }
-
     public static void main(String[] args) {
-        ArrayList<String> codes = generateCode();
-        for (String code : codes) {
+        // Generate and print 25 codes
+        for (String code : generateCode()) {
             System.out.println(code);
         }
     }
 }
+
